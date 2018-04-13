027889b7e2d8266bbe188edfe376ed9cdd081ad9
Tamilarasan Paneerselvam <tamilarasan@ideas2it.com>
2018-04-13 11:12:49 +0530

    Basic intents

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const Alexa = require('alexa-sdk');
 const Patient = require('./app/PatientModule.js');
 const Appointment = require('./app/Appointment.js');
 
-//console.log('======================', patient);
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
 const handlers = {
-    /*"LaunchRequest": 
-        welcomeNote(event.request, event.session,function(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                }),*/
     'BookAppointment': function () {
         var self = this;
         Appointment.createAppointment(self);
     }, 
     'LaunchRequest': function() {
         var self = this;
-        welcomeNote(this);
+        self.emit(":ask", "Hi, Welcome to ideamed, how can I help you?");
     },
     'GetAppointmentList': function() {
         var self = this;
         Appointment.getAppointments(self);
+    },
+    'AMAZON.HelpIntent': function() {
+        self.emit(":ask", "You can ask me to create appointment and know about your appointments.");
+    },
+    'AMAZON.StopIntent': function() {
+        self.emit(":tell", "Good Bye, Take Care");
     }
-    //"SessionEndedRequest":  sessionEnd()
 };
 
 exports.handler = function(event, context, callback) {
@@ -37,158 +37,3 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
-
-/*function sessionEnd() {
-    onSessionEnded(event.request, event.session);
-                context.succeed();
-}*/
-/*exports.handler = function (event, context) {
-    try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
-
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-         
-//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
-//         context.fail("Invalid Application ID");
-//      }
-
-       /* if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
-
-        switch(event.request.type) {
-            case "LaunchRequest": 
-                welcomeNote(event.request, event.session,function(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-                break;
-            case "IntentRequest": 
-                onIntent(event.request,event.session,function(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-                break;
-            case "SessionEndedRequest":
-                onSessionEnded(event.request, event.session);
-                context.succeed();
-                break;
-            default:
-                break;
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
-    }
-};*/
-
-/**
- * Called when the session starts.
- */
-/*function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // add any session init logic here
-}*/
-
-/**
- * Called when the user invokes the skill without specifying what they want.
- */
-function welcomeNote(self) {
-    console.log("onLaunch requestId=" + launchRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    var cardTitle = "Ideamed"
-    var speechOutput = "Hi, i am ideamed I can create and list appoinments for you."
-    //callback(session.attributes,
-      //  buildSpeechletResponse(cardTitle, speechOutput, "", true));
-    self.emit(":tell", speechOutput);
-}
-
-/**
- * Called when the user specifies an intent for this skill.
- */
-/*function onIntent(intentRequest, session, callback) {
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
-    switch(intentName) {
-        case "ListOfPatient":
-            Patient.getListOfPatient(intent, function(speechOutput, repromptText, sessionEnd) {
-                callback(session.attributes,
-                    buildSpeechletResponseWithoutCard(speechOutput, repromptText,  sessionEnd));
-            });
-            break;
-        case "BookAppointment";
-            Appointment.createAppointment(intent, function(speechOutput, repromptText, sessionEnd) {
-                callback(session.attributes,
-                    buildSpeechletResponseWithoutCard(speechOutput, repromptText,  sessionEnd));
-            });
-        default: 
-            break;
-    }
-}*/
-
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-/*function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // Add any cleanup logic here
-}
-
-function handleTestRequest(intent, session, callback) {
-    console.log(intent);
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Hello, World!", "eerererer", "true"));
-}*/
-
-// ------- Helper functions to build responses -------
-
-/*function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
-
-function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
-
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
-}*/
